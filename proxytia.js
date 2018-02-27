bbee3479f4dd718387b4bdb06830a92022d29598
ygoi <nrotolisator@gmail.com>
2018-02-28 03:14:57 +0700

    upload ex-3

---
+++ proxytia.js
@@ -0,0 +1,50 @@
+// Output untuk Input nama = '' dan peran = ''
+"Nama harus diisi!"
+
+var nama = '';{
+  alert('Nama harus diisi');
+}
+
+//Output untuk Input nama = 'Mikael' dan peran = ''
+"Halo Mikael, Pilih peranmu untuk memulai game!"
+
+var nama = 'Mikael';
+var peran = '';{
+  alert('Halo ' + nama + ', Pilih peranmu untuk memulai game!')
+}
+
+//Output untuk Input nama = 'Nina' dan peran 'Ksatria'
+"Selamat datang di Dunia Proxytia, Nina"
+"Halo Ksatria Nina, kamu dapat menyerang dengan senjatamu!"
+
+var nama = 'Nina';
+var peran = 'Ksatria'
+
+if(nama == 'Nina' && peran == 'Ksatria') {
+  alert('Selamat datang di Dunia Proxytia, Nina')
+  alert('Halo Ksatria Nina, kamu dapat menyerang dengan senjatamu!')
+}
+
+//Output untuk Input nama = 'Danu' dan peran 'Tabib'
+"Selamat datang di Dunia Proxytia, Danu"
+"Halo Tabib Danu, kamu akan membantu temanmu yang terluka."
+
+var nama = 'Danu';
+var peran = 'Tabib';
+
+if(nama == 'Danu' && peran == 'Tabib') {
+  alert('Selamat datang di Dunia Proxytia, Danu')
+  alert('Halo Tabib Danu, kamu dapat menyerang dengan senjatamu!')
+}
+
+//Output untuk Input nama = 'Zero' dan peran 'Penyihir'
+"Selamat datang di Dunia Proxytia, Zero"
+"Halo Penyihir Zero, ciptakan keajaiban yang membantu kemenanganmu!"
+
+var nama = 'Zero';
+var peran = 'Penyihir';
+
+if(nama == 'Zero' && peran == 'Penyihir') {
+  alert('Selamat datang di Dunia Proxytia, Zero')
+  alert('Halo Penyhir Zero, kamu dapat menyerang dengan senjatamu!')
+}
